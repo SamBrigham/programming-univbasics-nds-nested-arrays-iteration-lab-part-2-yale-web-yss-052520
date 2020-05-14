76bdee3d4eb4057e9a0745817630f2200b6d285b
Samuel Brigha <sam.brigham@yale.edu>
2020-05-14 19:07:35 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,6 @@ while row_index < src.count do
   element_index = 0
   min_of_nested_array = 
   while element_index < src[row_index].count do
-    binding.pry
     if src[row_index][element_index] < src[row_index][element_index -1]
     min_of_nested_array[row_index] = src[row_index][element_index]
     end
